711fa4781e2ab33e59aa84b38e4cdb112b331e76
EC2 Default User <ec2-user@ip-172-31-20-85.us-west-1.compute.internal>
2018-08-27 21:29:59 +0000

    Hold off on HTTPS. No site URL makes routing difficult.

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -18,6 +18,8 @@
  * @package WordPress
  */
 
+define('FORCE_SSL_ADMIN', true);
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
